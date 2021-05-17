9ab70f5d73aebd7e1422ffa1fb6f443e6c61e17a
DamonMok <damonmok1216@gmail.com>
2021-05-17 17:30:15 +0800

    登录布局-登录方式切换tab

---
+++ src/commons/element.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Select, Row, Col } from 'element-ui';
+
+Vue.use(Button)
+Vue.use(Row)
+Vue.use(Col)
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,10 +1,103 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+    <!-- Logo区域 -->
+    <img class="logo" src="~assets/img/login/logo.png" alt="" />
+
+    <!-- 登录区域 -->
+    <div class="login-container">
+      <!-- 登录方式切换区域 -->
+      <div class="login-tabs">
+        <div
+          class="type-tabs message-login"
+          :class="{ 'tab-active': isMessageLogin }"
+          @click="changeLoginType('type-message')"
+        >
+          免密码登录
+        </div>
+        <div
+          class="type-tabs password-login"
+          :class="{ 'tab-active': !isMessageLogin }"
+          @click="changeLoginType('type-password')"
+        >
+          密码登录
+        </div>
+        <img class="qrcode" src="~assets/img/login/qrcode.png" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isMessageLogin: true,
+    };
+  },
+  methods: {
+    changeLoginType(type) {
+      this.isMessageLogin = type === "type-message" ? true : false;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  // width: 100%;
+  height: 100%;
+  background-image: url("~assets/img/login/login_bg.png");
+  background-size: cover;
+  background-color: #b9e5f8;
+
+  .logo {
+    width: 128px;
+    height: 81px;
+    margin: 34px 0 24px 0;
+  }
+
+  .login-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 400px;
+    height: 496px;
+    background-color: #fff;
+
+    .login-tabs {
+      display: flex;
+      width: 352px;
+      height: 60px;
+
+      .type-tabs {
+        margin-right: 24px;
+        font-size: 16px;
+        line-height: 60px;
+        color: #444;
+        height: 60px;
+        font-weight: bold;
+
+        cursor: pointer;
+      }
+
+      .tab-active {
+        color: #121212;
+        font-weight: 600;
+        border-bottom: 3px solid #0066ff;
+      }
+
+      .qrcode {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 52px;
+        height: 52px;
+      }
+    }
+  }
+}
 </style>
