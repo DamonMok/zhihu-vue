47f06d954951431538b0c5d41bdc96f5e21c0276
DamonMok <damonmok1216@gmail.com>
2021-06-23 11:15:06 +0800

    首页-顶部卡片大图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
+    <!-- 导航栏 -->
     <Navigation></Navigation>
+
+    <!-- 主体区域 -->
+    <div class="main">
+      <!-- 左边区域 -->
+      <div class="left-side">
+        <!-- 顶部卡片大图区域 -->
+        <img class="top-card" src="https://pic2.zhimg.com/v2-fff33cded36aebe955c4806ae49e9d88.png" alt="">
+      </div>
+
+      <!-- 右边区域 -->
+      <div class="right-side"></div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +28,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.main {
+  width: 1032px;
+  height: 2000px;
+  margin: 0 auto;
+  margin-top: 10px;
+  background-color: #fbbbfb;
+
+  .left-side {
+    width: 697px;
+
+    .top-card {
+      margin-bottom: 10px;
+      border-radius: 2px;
+      box-shadow: 0 1px 3px rgba(18, 18, 18, 0.1);
+    }
+  }
+}
 </style>
